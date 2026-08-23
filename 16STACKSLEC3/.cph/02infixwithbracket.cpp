deb26374de27f15e1b8784e1ec536bea7b21ba64
saurav-patel700 <saurav392003@gmail.com>
2026-08-23 22:39:16 +0530

    add

---
+++ 16STACKSLEC3/.cph/02infixwithbracket.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int prio(char ch){
+    if(ch=='+' || ch=='-') return 1;
+    else return 2;
+}
+int solve(int val1,int val2,int ch){
+    if(ch=='+') return val1+val2;
+    else if(ch=='-') return val1-val2;
+    else if(ch=='*') return val1*val2;
+    else return val1/val2;
+}
+int main(){
+     string s="(7+9)*4/8-3";
+     stack<int> val;
+     stack<int> op;
+     for(int i=0;i<s.length();i++){
+        if(s[i]>='0' && s[i]<='9'){
+             val.push(s[i]-'0');
+        }
+            else if(s[i]=='(') op.push(s[i]);
+            else if(s[i]==')'){
+                while(op.top()!='('){
+                    char ch=op.top();
+                    op.pop();
+                    int val2=val.top();
+                    val.pop();
+                    int val1=val.top();
+                    val.pop();
+                    int ans=solve(val1,val2,ch);
+                     val.push(ans);
+                }
+                op.pop();   
+            }
+  else{
+    while(op.size()>0 && op.top()!='(' &&
+                prio(s[i])<=prio(op.top())){
+
+                char ch=op.top();
+                op.pop();
+
+                int val2=val.top();
+                val.pop();
+
+                int val1=val.top();
+                val.pop();
+
+                val.push(solve(val1,val2,ch));
+            }
+
+            op.push(s[i]);
+        }
+    } 
+
+    while(op.size()>0){
+                    char ch=op.top();
+                    op.pop();
+                    int val2=val.top();
+                    val.pop();
+                    int val1=val.top();
+                    val.pop();
+                    int ans=solve(val1,val2,ch);
+                    val.push(ans);
+    }
+    cout<<val.top()<<endl;
+    cout<<(7+9)*4/8-3;
+} 
